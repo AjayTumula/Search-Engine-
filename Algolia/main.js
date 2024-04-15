a054fa4351e7f74e521f1ae573ecc878373468be
AjayTumula <ajaytumula72@gmail.com>
2024-04-15 18:57:08 +0100

    Made necessary changes to the html file and main js

--- Algolia/main.js
+++ Algolia/main.js
@@ -1,4 +1,11 @@
 
+import algoliasearch from "algoliasearch";
+
+const client = algoliasearch("81B309EVRP", "c0c5f7f0c4fc2e3a984f078a5cfc404d");
+const index = client.initIndex("search");
+
+
+
 // Step One fetch the api to get product details.
 
 let data = [];
@@ -7,7 +14,7 @@ let resultsRootElement = document.querySelector('.results');
 
 fetch('https://fakestoreapi.com/products').then(res=>res.json()).then(json=>{
                 data = json
-                console.log(data)
+                // console.log(data)
             })
 
 //Step two, add event to the search bar 
@@ -15,9 +22,12 @@ fetch('https://fakestoreapi.com/products').then(res=>res.json()).then(json=>{
 document.querySelector('#searchInput').addEventListener('keyup', ()=> {
     let searchTerm = document.querySelector('#searchInput').value;
     let resultsArray = [];
+
     if(String(searchTerm).trim().length > 0) {
-      resultsArray = data.filter(product=> String(product.title).includes(searchTerm))
-      renederProducts(resultsArray);
+
+     index.search(searchTerm).then(response=> {
+      renederProducts(response.hits);
+     })
     } else {
       removeElements();
     } 
@@ -60,4 +70,25 @@ function removeElements(){
   })
 }
 
+function addNewElement() {
+  index.saveObject(
+    {
+      objectID: 51,
+      "id": 1,
+      "title": "Ajay Favourite T-shirt",
+      "price": 109.95,
+      "description": "This is my favourite Armani t-shirt",
+      "category": "men's clothing",
+      "image": "https://picsum.photos/200",
+      "rating": {
+          "rate": 3.9,
+          "count": 120
+      }
+  }
+  ).then(response=> {
+    console.log(response)
+  })
+}
+
+addNewElement()
 
